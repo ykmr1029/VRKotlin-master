caeaf7c418686e124773084141ec463d2623ceb4
maru <maru1029384756@gmail.com>
2018-02-01 02:58:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# License
+
+## Apache Licens Version 2.0
+Copyright (c) 2017 Leverages 
+see http://www.apache.org/licenses/
+# XylitolBottleGumApp
+# XylitolBottleGumApp
---
+++ app/src/main/java/com/leverages/imagesearchbluemix/Secret.kt
@@ -0,0 +1,22 @@
+package com.leverages.imagesearchbluemix
+
+/**
+ * Created by takeo.kusama on 2017/07/26.
+ */
+
+
+
+val WATSON_API_KEY = "9c5a2619821e54cb0c82012b8a75d751d4b89f6b"
+val VR_USE_CLASSIFIERS = listOf("XylitolBottleGum")
+
+/*
+* Your AWS Access Key ID, as taken from the AWS Your Account page.
+*/
+val AWS_ACCESS_KEY_ID = "AKIAIM6EPFLRB2BOWY5A"
+/*
+ * Your AWS Secret Key corresponding to the above ID, as taken from the AWS
+ * Your Account page.
+ */
+val AWS_SECRET_KEY = "NLIEKRp4qtnRB3Lg/AtfZeGor4/rJUFBGj+idlFP"
+
+val ASSOCIATE_TAG = "ykmr1029-22"
